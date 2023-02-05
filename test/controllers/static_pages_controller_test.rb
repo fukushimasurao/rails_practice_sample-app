982a2637e475ae8223d5c1b3194489a022ba9e2f
Hayato Fukushima <nu0640042@gmail.com>
2023-02-05 23:43:10 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,15 +7,15 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
+  # test "should get root" do
+  #   get root_url
+  #   assert_response :success
+  # end
 
   test "should get home" do
     # getメソッドで、 'static_pages/home'にアクセスしたときに、200が帰ってくるか。
     # static_pages_home_urlはrouting を作成したときに、勝手に作られる。
-    get static_pages_home_url
+    get root_path
     assert_response :success
     
     # HTMLのタイトルに、想定するタイトルがあるか確認する。
@@ -23,21 +23,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
   
   
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
